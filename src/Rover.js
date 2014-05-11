7d8814f2d7a0864eaa15ab8d35d3d548790179b8
Edwin Vasquez <edwin.vasquez@newshore.es>
2014-05-10 22:38:07 -0500

    Finish solution in Strategy Pattern, by function way

--- src/Rover.js
+++ src/Rover.js
@@ -11,16 +11,8 @@ Rover.prototype.receive = function(commands){
 		var x = this.position.x,
 			y = this.position.y;
 		
-		this.position = //Se debe ejecutar una de las funciones strategy.
-		/*if(command === "l"){
-			this.position = new Position(x-1, y);
-		} else if (command === "r") {
-			this.position = new Position(x+1, y);
-		} else if (command === "b") {
-			this.position = new Position(x, y-1);
-		} else { //forward
-			this.position = new Position(x, y+1);
-		}*/
+		var moveRover = new MovementRover(command, x, y);
+		this.position = moveRover.Execute();
 	}
 }
 
--- src/Strategy.js
+++ src/Strategy.js
@@ -1,23 +1,23 @@
 var MovementRover = function(strategy) {
     this.strategy = strategy;
-};
+}
 
-Movement.prototype.execute = function() {
+MovementRover.prototype.execute = function() {
     return this.strategy();
-};
+}
 
 var LeftMove = function() {
     return new Position(x-1, y);
-};
+}
 
 var RightMove = function() {
     return new Position(x+1, y);
-};
+}
 
 var BackwardMove = function() {
     return new Position(x, y-1);
-};
+}
 
 var ForwardMove = function() {
     return new Position(x, y+1);
-};
+}
